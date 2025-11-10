a9c794cbcd7e86315e6a8a6f929b170f70005892
Yuhong11111 <nieyuhong2000@gmail.com>
2025-11-10 17:09:28 -0500

    test update

--- fullstack-app/frontend/src/pages/__tests__/Login.test.tsx
+++ fullstack-app/frontend/src/pages/__tests__/Login.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { Login } from '../login';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { system } from '../../theme';
 
 test('renders login form', () => {
     render(
-        <BrowserRouter>
-            <Login />
-        </BrowserRouter>
+        <ChakraProvider value={system}>
+            <BrowserRouter>
+                <Login />
+            </BrowserRouter>
+        </ChakraProvider>
     );
 
     expect(screen.getByPlaceholderText(/username/i)).toBeInTheDocument();
--- fullstack-app/frontend/src/setupTests.ts
+++ fullstack-app/frontend/src/setupTests.ts
@@ -4,6 +4,7 @@ import { TextDecoder, TextEncoder } from 'util';
 const globalShim = globalThis as typeof globalThis & {
   TextEncoder?: typeof TextEncoder;
   TextDecoder?: typeof TextDecoder;
+  structuredClone?: typeof structuredClone;
 };
 
 if (!globalShim.TextEncoder) {
@@ -12,4 +13,8 @@ if (!globalShim.TextEncoder) {
 
 if (!globalShim.TextDecoder) {
   globalShim.TextDecoder = TextDecoder as typeof TextDecoder;
+}
+
+if (!globalShim.structuredClone) {
+  globalShim.structuredClone = <T>(value: T): T => JSON.parse(JSON.stringify(value));
 }
